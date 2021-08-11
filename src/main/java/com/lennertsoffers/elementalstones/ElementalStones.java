405c27aac8eadde92bc7f9695f78a737d8d06496
Lennert Soffers <r0833969@student.thomasmore.be>
2021-08-11 20:31:12 +0200

    defenseStoneMove8 and passive defense stick done

--- src/main/java/com/lennertsoffers/elementalstones/ElementalStones.java
+++ src/main/java/com/lennertsoffers/elementalstones/ElementalStones.java
@@ -1,6 +1,7 @@
 package com.lennertsoffers.elementalstones;
 
 import com.lennertsoffers.elementalstones.event.*;
+import com.lennertsoffers.elementalstones.stones.earthStone.DefenseStone;
 import org.bukkit.plugin.java.JavaPlugin;
 import com.lennertsoffers.elementalstones.items.ItemStones;
 
@@ -14,6 +15,8 @@ public final class ElementalStones extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new ClickEvent(), this);
         getServer().getPluginManager().registerEvents(new PlayerJoinEvent(), this);
         getServer().getPluginManager().registerEvents(new PlayerQuitEvent(), this);
+
+        new DefenseStone(this);
     }
 
     @Override
--- src/main/java/com/lennertsoffers/elementalstones/stones/earthStone/DefenseStone.java
+++ src/main/java/com/lennertsoffers/elementalstones/stones/earthStone/DefenseStone.java
@@ -1,4 +1,73 @@
 package com.lennertsoffers.elementalstones.stones.earthStone;
 
+import com.lennertsoffers.elementalstones.ElementalStones;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitScheduler;
+
 public class DefenseStone {
+
+    private final ElementalStones plugin;
+
+    public DefenseStone(ElementalStones plugin) {
+        this.plugin = plugin;
+        BukkitScheduler scheduler = plugin.getServer().getScheduler();
+        scheduler.scheduleSyncRepeatingTask(plugin, this::defenseStonePassive, 0L, 200L);
+    }
+
+    // PASSIVE
+    private void defenseStonePassive() {
+        for (Player player : this.plugin.getServer().getOnlinePlayers()) {
+            player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 220, 1, true, true, true));
+        }
+    }
+
+    // MOVE 8
+    public static void defenseStoneMove8(Player player) {
+        Location location = player.getLocation();
+        World world = player.getWorld();
+        int playerX = location.getBlockX();
+        int playerY = location.getBlockY();
+        int playerZ = location.getBlockZ();
+        if (world.getBlockAt(location.add(0, -1, 0)).getType() != Material.AIR) {
+            location.add(0, 1, 0);
+            for (int i = 0; i < 3; i++) {
+                world.getBlockAt(playerX + 2, playerY + i, playerZ - 1).setType(Material.STONE);
+                world.getBlockAt(playerX + 2, playerY + i, playerZ).setType(Material.STONE);
+                world.getBlockAt(playerX + 2, playerY + i, playerZ + 1).setType(Material.STONE);
+                world.getBlockAt(playerX - 1, playerY + i, playerZ + 2).setType(Material.STONE);
+                world.getBlockAt(playerX, playerY + i, playerZ + 2).setType(Material.STONE);
+                world.getBlockAt(playerX + 1, playerY + i, playerZ + 2).setType(Material.STONE);
+                world.getBlockAt(playerX - 2, playerY + i, playerZ - 1).setType(Material.STONE);
+                world.getBlockAt(playerX - 2, playerY + i, playerZ).setType(Material.STONE);
+                world.getBlockAt(playerX - 2, playerY + i, playerZ + 1).setType(Material.STONE);
+                world.getBlockAt(playerX - 1, playerY + i, playerZ - 2).setType(Material.STONE);
+                world.getBlockAt(playerX, playerY + i, playerZ - 2).setType(Material.STONE);
+                world.getBlockAt(playerX + 1, playerY + i, playerZ - 2).setType(Material.STONE);
+
+                world.getBlockAt(playerX + 1, playerY + i, playerZ + 1).setType(Material.AIR);
+                world.getBlockAt(playerX + 1, playerY + i, playerZ).setType(Material.AIR);
+                world.getBlockAt(playerX + 1, playerY + i, playerZ - 1).setType(Material.AIR);
+                world.getBlockAt(playerX, playerY + i, playerZ + 1).setType(Material.AIR);
+                world.getBlockAt(playerX, playerY + i, playerZ).setType(Material.AIR);
+                world.getBlockAt(playerX, playerY + i, playerZ - 1).setType(Material.AIR);
+                world.getBlockAt(playerX - 1, playerY + i, playerZ + 1).setType(Material.AIR);
+                world.getBlockAt(playerX - 1, playerY + i, playerZ).setType(Material.AIR);
+                world.getBlockAt(playerX - 1, playerY + i, playerZ - 1).setType(Material.AIR);
+            }
+            world.getBlockAt(playerX + 1, playerY + 3, playerZ + 1).setType(Material.STONE);
+            world.getBlockAt(playerX + 1, playerY + 3, playerZ).setType(Material.STONE);
+            world.getBlockAt(playerX + 1, playerY + 3, playerZ - 1).setType(Material.STONE);
+            world.getBlockAt(playerX, playerY + 3, playerZ + 1).setType(Material.STONE);
+            world.getBlockAt(playerX, playerY + 3, playerZ).setType(Material.STONE);
+            world.getBlockAt(playerX, playerY + 3, playerZ - 1).setType(Material.STONE);
+            world.getBlockAt(playerX - 1, playerY + 3, playerZ + 1).setType(Material.STONE);
+            world.getBlockAt(playerX - 1, playerY + 3, playerZ).setType(Material.STONE);
+            world.getBlockAt(playerX - 1, playerY + 3, playerZ - 1).setType(Material.STONE);
+        }
+    }
 }
