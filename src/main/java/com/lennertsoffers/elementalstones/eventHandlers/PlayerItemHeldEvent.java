bbc7c823f100a33d4ed46273273bebc2b7aaffe9
lennertsoffers <lennert.soffers@hotmail.com>
2021-08-23 19:52:01 +0200

    Mining Stone move4 done

--- src/main/java/com/lennertsoffers/elementalstones/eventHandlers/PlayerItemHeldEvent.java
+++ src/main/java/com/lennertsoffers/elementalstones/eventHandlers/PlayerItemHeldEvent.java
@@ -3,7 +3,9 @@ package com.lennertsoffers.elementalstones.eventHandlers;
 import com.lennertsoffers.elementalstones.customClasses.ActivePlayer;
 import com.lennertsoffers.elementalstones.stones.earthStone.DefenseStone;
 
+import com.lennertsoffers.elementalstones.stones.earthStone.EarthStone;
 import com.lennertsoffers.elementalstones.stones.earthStone.EarthbendingStone;
+import com.lennertsoffers.elementalstones.stones.earthStone.MiningStone;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -17,9 +19,9 @@ public class PlayerItemHeldEvent implements Listener {
         ActivePlayer activePlayer = ActivePlayer.getActivePlayer(player.getUniqueId());
         if (activePlayer != null) {
             if (event.getNewSlot() == 0) {
-                EarthbendingStone.move8(activePlayer);
+                EarthStone.move2(activePlayer);
             } else if (event.getNewSlot() == 1) {
-                EarthbendingStone.move4(activePlayer);
+                MiningStone.move4(activePlayer);
             } else if (event.getNewSlot() == 4) {
                 System.out.println("test");
                 DefenseStone.move7(player);
