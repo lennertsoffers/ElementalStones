61b7291bc93c36f7211d4e03dcc67d010a6de261
lennertsoffers <lennert.soffers@hotmail.com>
2021-08-24 21:44:33 +0200

    FireStone move2 done

--- src/main/java/com/lennertsoffers/elementalstones/eventHandlers/ClickEvent.java
+++ src/main/java/com/lennertsoffers/elementalstones/eventHandlers/ClickEvent.java
@@ -1,16 +1,31 @@
 package com.lennertsoffers.elementalstones.eventHandlers;
 
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.PacketContainer;
 import com.lennertsoffers.elementalstones.ElementalStones;
 import com.lennertsoffers.elementalstones.customClasses.ActivePlayer;
+import com.lennertsoffers.elementalstones.customClasses.StaticVariables;
 import com.lennertsoffers.elementalstones.items.ItemStones;
 import com.lennertsoffers.elementalstones.stones.earthStone.EarthbendingStone;
 import com.lennertsoffers.elementalstones.stones.windStone.AgilityStone;
+import net.minecraft.server.PacketPlayOutWorldParticles;
+import net.minecraft.server.ParticleType;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.Server;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Random;
 
 public class ClickEvent implements Listener {
 
@@ -21,7 +36,7 @@ public class ClickEvent implements Listener {
     }
 
     @EventHandler
-    public void onClick(PlayerInteractEvent event) {
+    public void onClick(PlayerInteractEvent event) throws InvocationTargetException {
         Player player = event.getPlayer();
         if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
             if (ItemStones.allStones.contains(player.getInventory().getItemInMainHand())) {
@@ -38,6 +53,46 @@ public class ClickEvent implements Listener {
             }
         }
 
-        if (event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK) {}
+        if (event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK) {
+//            Location location = player.getLocation().add(player.getLocation().getDirection()).add(0, 1, 0);
+//            ProtocolManager manager = ProtocolLibrary.getProtocolManager();
+//            PacketContainer packet = manager.createPacket(PacketType.Play.Server.WORLD_PARTICLES);
+//            packet.getIntegers().write(0, 77);
+////            packet.getIntegers().write(1, 10);
+//            packet.getDoubles().write(0, location.getX());
+//            packet.getDoubles().write(1, location.getY());
+//            packet.getDoubles().write(2, location.getZ());
+//            packet.getFloat().write(0, 0F);
+//            packet.getFloat().write(1, 0F);
+//            packet.getFloat().write(2, 0F);
+//            packet.getFloat().write(3, 0F);
+//            packet.getBooleans().write(0, false);
+//            try {
+//                manager.sendServerPacket(player, packet);
+//            } catch (InvocationTargetException exception) {
+//                System.out.println(exception.getMessage());
+//            }
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/lennertsoffers/elementalstones/stones/fireStone/FireStone.java
+++ src/main/java/com/lennertsoffers/elementalstones/stones/fireStone/FireStone.java
@@ -8,11 +8,14 @@ import org.bukkit.Particle;
 import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Random;
 
@@ -20,7 +23,7 @@ public class FireStone {
 
     // MOVE 1
     // A-Quick-Snack
-    // -> Turns every rawww stack of food to the cooked version of it
+    // -> Turns every raw stack of food to the cooked version of it
     public static void move1(ActivePlayer activePlayer) {
         Player player = activePlayer.getPlayer();
         for (ItemStack itemStack : player.getInventory().getContents()) {
@@ -49,19 +52,48 @@ public class FireStone {
 
     // MOVE 2
     // Floating Fire
-    // -> You can summon a fireball and hold it right in front of you
+    // -> You can summon a fireball and hold it right in front of you which damages entities
     public static void move2(ActivePlayer activePlayer) {
         Player player = activePlayer.getPlayer();
         World world = player.getWorld();
         Random random = new Random();
         new BukkitRunnable() {
+            int ticksLived = 1200;
+            final Map<LivingEntity, Long> damagedEntityCooldown = new HashMap<>();
             @Override
             public void run() {
-                Location location = player.getLocation().add(0, 1.5, 0);
-                location.add(location.getDirection());
-                for (int i = 0; i < 200; i++) {
-                    world.spawnParticle(Particle.FLAME, location, 0, random.nextDouble() / 20, random.nextDouble() / 20, random.nextDouble() / 20, 0);
+                Vector direction = player.getLocation().getDirection();
+                Location location = player.getLocation().add(direction.getX() * 2.5, direction.getY() * 2.5, direction.getZ() * 2.5).add(0, 1.5, 0);
+                double nominator = ticksLived / 200f;
+                for (Entity entity : world.getNearbyEntities(location, 0.5, 0.5, 0.5)) {
+                    if (entity != null) {
+                        if (entity instanceof LivingEntity) {
+                            LivingEntity livingEntity = (LivingEntity) entity;
+                            if (livingEntity != player) {
+                                if (damagedEntityCooldown.containsKey(livingEntity)) {
+                                    if (damagedEntityCooldown.get(livingEntity) < System.currentTimeMillis()) {
+                                        entity.setFireTicks(200);
+                                        ((LivingEntity) entity).damage(10 / nominator);
+                                        damagedEntityCooldown.replace(livingEntity, System.currentTimeMillis() + (10 * 1000));
+                                    }
+                                } else {
+                                    entity.setFireTicks(200);
+                                    ((LivingEntity) entity).damage(10 / nominator);
+                                    damagedEntityCooldown.put(livingEntity, System.currentTimeMillis() + (10 * 1000));
+                                }
+                            }
+                        }
+                    }
                 }
+                for (int i = 0; i < 30; i++) {
+                    Vector fireballDirection = player.getLocation().getDirection();
+                    Location fireballLocation = player.getLocation().add(fireballDirection.getX() * 2.5, fireballDirection.getY() * 2.5, direction.getZ() * 2.5).add(0, 1.5, 0);
+                    world.spawnParticle(Particle.FLAME, fireballLocation.add(random.nextGaussian() / nominator, random.nextGaussian() / nominator, random.nextGaussian() / nominator), 0, 0, 0, 0);
+                }
+                if (ticksLived > 3000) {
+                    this.cancel();
+                }
+                ticksLived++;
             }
         }.runTaskTimer(StaticVariables.plugin, 0L, 1L);
     }
