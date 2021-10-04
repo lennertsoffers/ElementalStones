d9dab801fd6514d5cf78cc0ab191c81ce798136b
lennertsoffers <lennert.soffers@hotmail.com>
2021-10-04 12:43:17 +0200

    AgilityStone move6: Done

--- src/main/java/com/lennertsoffers/elementalstones/stones/windStone/AgilityStone.java
+++ src/main/java/com/lennertsoffers/elementalstones/stones/windStone/AgilityStone.java
@@ -4,6 +4,7 @@ import com.lennertsoffers.elementalstones.customClasses.ActivePlayer;
 import com.lennertsoffers.elementalstones.customClasses.StaticVariables;
 import org.bukkit.*;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerToggleFlightEvent;
 import org.bukkit.potion.PotionEffect;
@@ -11,9 +12,47 @@ import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class AgilityStone {
+
+    private static void damageOnDash(ActivePlayer activePlayer) {
+        ArrayList<LivingEntity> damagedEntities = new ArrayList<>();
+        Player player = activePlayer.getPlayer();
+        new BukkitRunnable() {
+            int amountOfTicks = 0;
+            @Override
+            public void run() {
+                if (!player.getWorld().getNearbyEntities(player.getLocation(), 0.5, 0.5, 0.5).isEmpty()) {
+                    for (Entity entity : player.getWorld().getNearbyEntities(player.getLocation(), 0.5, 0.5, 0.5)) {
+                        if (entity != null) {
+                            if (entity instanceof LivingEntity) {
+                                LivingEntity livingEntity = (LivingEntity) entity;
+                                if (livingEntity != player) {
+                                    if (!damagedEntities.contains(livingEntity)) {
+                                        damagedEntities.add(livingEntity);
+                                        if (activePlayer.isInAirBoost()) {
+                                            livingEntity.damage(6);
+                                            player.getWorld().playSound(player.getLocation(), Sound.ENTITY_PLAYER_HURT, 1, 1);
+                                        } else {
+                                            livingEntity.damage(2);
+                                            player.getWorld().playSound(player.getLocation(), Sound.ENTITY_PLAYER_HURT, 1, 1);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                if (amountOfTicks > 10) {
+                    this.cancel();
+                }
+                amountOfTicks++;
+            }
+        }.runTaskTimer(StaticVariables.plugin, 0L, 1L);
+    }
 
     // PASSIVE
     // Double Jump
@@ -39,18 +78,28 @@ public class AgilityStone {
 
     // MOVE 4
     // Forward Dash
-    // -> Player dashes forwards
+    // -> Player dashes forwards and damages entities along the way
     public static void move4(ActivePlayer activePlayer) {
         Player player = activePlayer.getPlayer();
-        player.setVelocity(player.getLocation().getDirection().multiply(2).setY(0.1));
+        damageOnDash(activePlayer);
+        int multiplier = 2;
+        if (activePlayer.isInAirBoost()) {
+            multiplier = 4;
+        }
+        player.setVelocity(player.getLocation().getDirection().multiply(multiplier).setY(0.1));
     }
 
     // MOVE 5
     // Backward Dash
-    // -> Player dashes backwards
+    // -> Player dashes backwards and damages entities along the way
     public static void move5(ActivePlayer activePlayer) {
         Player player = activePlayer.getPlayer();
-        player.setVelocity(player.getLocation().getDirection().multiply(-2).setY(0.1));
+        damageOnDash(activePlayer);
+        int multiplier = 2;
+        if (activePlayer.isInAirBoost()) {
+            multiplier = 4;
+        }
+        player.setVelocity(player.getLocation().getDirection().multiply(-multiplier).setY(0.1));
     }
 
     // MOVE 6
@@ -60,7 +109,7 @@ public class AgilityStone {
     // -> Activate ability again to create the smoke screen or wait till the ball collides with a block or entity
     public static void move6(ActivePlayer activePlayer) {
         Player player = activePlayer.getPlayer();
-        Location startingLocation = player.getLocation().add(player.getLocation().getDirection().multiply(2)).add(0, 1, 0);
+        Location startingLocation = player.getLocation().add(player.getLocation().getDirection()).add(0, 1, 0);
         final Location[] impactLocation = {player.getLocation()};
         BukkitRunnable smoke = new BukkitRunnable() {
             int amountOfTicks = 0;
@@ -124,7 +173,7 @@ public class AgilityStone {
         Player player = activePlayer.getPlayer();
         if ((int) activePlayer.getCharge() == -1) {
             activePlayer.setChargingStart();
-            player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 1000, 10, true, false, false));
+            player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 1000, 3, true, false, false));
             activePlayer.setMove7LaunchState(1);
             new BukkitRunnable() {
                 @Override
@@ -136,7 +185,11 @@ public class AgilityStone {
                 }
             }.runTaskLater(StaticVariables.plugin, 90);
         } else {
-            double velocityY = activePlayer.getCharge() / 1000;
+            int nominator = 2000;
+            if (activePlayer.isInAirBoost()) {
+                nominator = 1000;
+            }
+            double velocityY = activePlayer.getCharge() / nominator;
             activePlayer.resetCharge();
             player.removePotionEffect(PotionEffectType.SLOW);
             activePlayer.setMove7LaunchState(2);
@@ -145,6 +198,18 @@ public class AgilityStone {
     }
 
     // MOVE 8
+    // Air Boost
+    // -> Makes the player run faster
+    // -> Makes the player jump higher
+    // -> Dashes do more damage
+    // -> Charge jump charges faster
+    // -> Cooldowns shorter
+    public static void move8(ActivePlayer activePlayer) {
+        Player player = activePlayer.getPlayer();
+        activePlayer.activateAirBoost();
+        player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 1200, 3, false, false, false));
+        player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 1200, 1, false, false, false));
+    }
 }
 
 
