db0f4bca4aa38aa03415f9372b5578056afd8f75
lennertsoffers <lennert.soffers@hotmail.com>
2022-02-04 23:04:42 +0100

    ExplosionStone: Move5 easter egg done

--- src/main/java/com/lennertsoffers/elementalstones/stones/fireStone/ExplosionStone.java
+++ src/main/java/com/lennertsoffers/elementalstones/stones/fireStone/ExplosionStone.java
@@ -4,98 +4,31 @@ import com.lennertsoffers.elementalstones.customClasses.models.ActivePlayer;
 import com.lennertsoffers.elementalstones.customClasses.StaticVariables;
 import com.lennertsoffers.elementalstones.customClasses.tools.CheckLocationTools;
 import com.lennertsoffers.elementalstones.customClasses.tools.MathTools;
+import com.lennertsoffers.elementalstones.customClasses.tools.RandomiserTools;
 import com.lennertsoffers.elementalstones.stones.earthStone.EarthStone;
 import org.bukkit.*;
+import org.bukkit.block.Block;
 import org.bukkit.enchantments.Enchantment;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.*;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
 public class ExplosionStone extends FireStone {
 
-    private static ItemStack addEnchantments(ItemStack item) {
-        ItemMeta itemMeta = item.getItemMeta();
-        if (itemMeta != null) {
-            itemMeta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 5, true);
-            itemMeta.addEnchant(Enchantment.PROTECTION_EXPLOSIONS, 5, true);
-            itemMeta.addEnchant(Enchantment.PROTECTION_FALL, 5, true);
-            itemMeta.addEnchant(Enchantment.PROTECTION_FIRE, 5, true);
-            itemMeta.addEnchant(Enchantment.PROTECTION_PROJECTILE, 5, true);
-            itemMeta.addEnchant(Enchantment.BINDING_CURSE, 1, true);
-            item.setItemMeta(itemMeta);
-        }
-        return item;
-    }
-
-    private static void buildPerpendicularWallX(Location location, World world) {
-        location.add(0, -1, 3);
-        for (int i = 0; i < 15; i++) {
-            if (i % 5 == 0) {
-                location.add(0, 1, -5);
-            }
-            world.getBlockAt(location).setType(Material.STONE);
-            location.add(0, 0, 1);
-        }
-    }
-
-    private static void buildPerpendicularWallZ(Location location, World world) {
-        location.add(3, -1, 0);
-        for (int i = 0; i < 15; i++) {
-            if (i % 5 == 0) {
-                location.add(-5, 1, 0);
-            }
-            world.getBlockAt(location).setType(Material.STONE);
-            location.add(1, 0, 0);
-        }
-    }
-
-    private static void buildDiagonalWallX(Location location, World world) {
-        location.add(3, -1, 3);
-        for (int i = 0; i < 15; i++) {
-            if (i % 5 == 0) {
-                location.add(-5, 1, -5);
-            }
-            world.getBlockAt(location).setType(Material.STONE);
-            location.add(1, 0, 1);
-        }
-    }
-
-    private static void buildDiagonalWallZ(Location location, World world) {
-        location.add(3, -1, -3);
-        for (int i = 0; i < 15; i++) {
-            if (i % 5 == 0) {
-                location.add(-5, 1, 5);
-            }
-            world.getBlockAt(location).setType(Material.STONE);
-            location.add(1, 0, -1);
-        }
-    }
-
-    // PASSIVE
-    // Stone Resistance
-    // -> Player gets the resistance effect when he is holding the stone
-    private static void passive() {
-        StaticVariables.scheduler.scheduleSyncRepeatingTask(StaticVariables.plugin, () -> {
-            for (ActivePlayer activePlayer : ActivePlayer.getActivePlayers()) {
-                Player player = activePlayer.getPlayer();
-                player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 220, 1, true, true, true));
-            }
-        }, 0L, 200L);
-    }
-
     // MOVE 4
     // Smoke Screen
     // -> Following up to flying rock
-    // -> Explodes the flying block into a big smoke which makes it impossible to see trough
+    // -> Explodes the flying block into a big smoke which makes it impossible to see through
     public static Runnable move4(ActivePlayer activePlayer) {
         return () -> {
             Player player = activePlayer.getPlayer();
@@ -173,32 +106,64 @@ public class ExplosionStone extends FireStone {
     public static Runnable move5(ActivePlayer activePlayer) {
         return () -> {
             Player player = activePlayer.getPlayer();
-            ItemStack helmet;
-            ItemStack boots;
-            ItemStack chestplate;
-            ItemStack leggings;
+            World world = player.getWorld();
+            Location location = player.getLocation();
 
-            helmet = player.getInventory().getHelmet();
-            chestplate = player.getInventory().getChestplate();
-            leggings = player.getInventory().getLeggings();
-            boots = player.getInventory().getBoots();
+            if (player.isSneaking()) {
+                Vector direction = location.getDirection().setY(0);
+                Vector perpendicularDirection = direction.clone().rotateAroundY(Math.PI / 2);
 
-            ItemStack betterHelmet = ExplosionStone.addEnchantments(new ItemStack(Material.NETHERITE_HELMET));
-            ItemStack betterChestplate = ExplosionStone.addEnchantments(new ItemStack(Material.NETHERITE_CHESTPLATE));
-            ItemStack betterLeggings = ExplosionStone.addEnchantments(new ItemStack(Material.NETHERITE_LEGGINGS));
-            ItemStack betterBoots = ExplosionStone.addEnchantments(new ItemStack(Material.NETHERITE_BOOTS));
+                location.add(direction.clone().multiply(20));
+                Location finalCenterLocation = location.clone();
+                location.add(perpendicularDirection.clone().multiply(-10));
 
-            player.getInventory().setHelmet(betterHelmet);
-            player.getInventory().setChestplate(betterChestplate);
-            player.getInventory().setLeggings(betterLeggings);
-            player.getInventory().setBoots(betterBoots);
+                new BukkitRunnable() {
+                    int rows = 1;
 
-            StaticVariables.scheduler.scheduleSyncDelayedTask(StaticVariables.plugin, () -> {
-                player.getInventory().setHelmet(helmet);
-                player.getInventory().setChestplate(chestplate);
-                player.getInventory().setLeggings(leggings);
-                player.getInventory().setBoots(boots);
-            }, 1200L);
+                    @Override
+                    public void run() {
+                        for (int i = 1; i < 20; i++) {
+                            Location fireworkLocation = location.clone().add(perpendicularDirection.clone().multiply(i)).add(direction.clone().multiply(rows));
+                            fireworkLocation.setY(world.getHighestBlockYAt(location) + 1);
+
+                            Firework firework = (Firework) world.spawnEntity(fireworkLocation, EntityType.FIREWORK);
+
+                            FireworkMeta fireworkMeta = firework.getFireworkMeta();
+                            fireworkMeta.setPower(1);
+
+                            ArrayList<Color> colors = new ArrayList<>();
+                            ArrayList<Color> fadeColors = new ArrayList<>();
+                            for (int k = 0; k < StaticVariables.random.nextInt(3) + 1; k++) {
+                                colors.add(RandomiserTools.randomColor());
+                            }
+                            for (int k = 0; k < StaticVariables.random.nextInt(3) + 1; k++) {
+                                fadeColors.add(RandomiserTools.randomColor());
+                            }
+
+                            if (player.isSneaking()) {
+                                fireworkMeta.addEffect(FireworkEffect.builder().with(RandomiserTools.randomEnum(FireworkEffect.Type.class)).withColor(colors).withFade(fadeColors).flicker(StaticVariables.random.nextBoolean()).trail(StaticVariables.random.nextBoolean()).build());
+                            } else {
+                                fireworkMeta.addEffect(FireworkEffect.builder().withColor(colors).withFade(fadeColors).flicker(StaticVariables.random.nextBoolean()).trail(StaticVariables.random.nextBoolean()).build());
+                            }
+
+                            firework.setFireworkMeta(fireworkMeta);
+                        }
+
+                        rows++;
+                        if (rows > 10) {
+                            this.cancel();
+                            finalCenterLocation.setY(world.getHighestBlockYAt(finalCenterLocation) + 1);
+                            Firework firework = (Firework) world.spawnEntity(finalCenterLocation, EntityType.FIREWORK);
+                            FireworkMeta fireworkMeta = firework.getFireworkMeta();
+                            fireworkMeta.setPower(1);
+                            fireworkMeta.addEffect(FireworkEffect.builder().with(FireworkEffect.Type.BALL_LARGE).withColor(Color.RED, Color.BLUE).withFade(Color.BLACK).flicker(false).trail(true).build());
+                            firework.setFireworkMeta(fireworkMeta);
+                        }
+                    }
+                }.runTaskTimer(StaticVariables.plugin, 0L, 50L);
+            } else {
+
+            }
         };
     }
 
@@ -209,24 +174,6 @@ public class ExplosionStone extends FireStone {
         return () -> {
             Player player = activePlayer.getPlayer();
             Location location = player.getLocation();
-            float yaw = location.getYaw();
-            if (yaw > -25 && yaw < 25) {
-                System.out.println("1");
-            } else if (yaw >= 25 && yaw < 65) {
-                System.out.println("2");
-            } else if (yaw >= 65 && yaw < 115) {
-                System.out.println("3");
-            } else if (yaw >= 115 && yaw < 155) {
-                System.out.println("4");
-            } else if (yaw < -155 || yaw > 155) {
-                System.out.println("5");
-            } else if (yaw <= -25 && yaw > -65) {
-                System.out.println("6");
-            } else if (yaw <= -65 && yaw > -115) {
-                System.out.println("7");
-            } else {
-                System.out.println("8");
-            }
         };
     }
 
@@ -237,32 +184,6 @@ public class ExplosionStone extends FireStone {
         return () -> {
             Player player = activePlayer.getPlayer();
             Location location = player.getLocation();
-            List<Entity> entities = player.getNearbyEntities(3, 0, 3);
-            int playerX = location.getBlockX() - 3;
-            int playerY = location.getBlockY();
-            int playerZ = location.getBlockZ() - 3;
-
-            for (int i = 0; i < 7; i++) {
-                for (int j = 0; j < 7; j++) {
-                    if (!((i == 0 && j == 0) || (i == 0 && j == 6) || (i == 6 && j == 0) || (i == 6 && j == 6))) {
-                        ItemStack stack;
-                        if (player.getWorld().getBlockAt(player.getLocation().add(0, -1, 0)).getType().equals(Material.AIR)) {
-                            stack = new ItemStack(Material.STONE);
-                        } else {
-                            stack = new ItemStack(player.getWorld().getBlockAt(player.getLocation().add(0, -1, 0)).getType());
-                        }
-                        player.getWorld().spawnParticle(Particle.ITEM_CRACK, playerX + i, playerY + 0.5, playerZ + j, 50, 1, 0, 1, 0.1, stack);
-                        for (Entity entity : entities) {
-                            try {
-                                LivingEntity livingEntity = (LivingEntity) entity;
-                                livingEntity.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 140, 10));
-                            } finally {
-                                entity.setVelocity(new Vector(0, 0.5, 0));
-                            }
-                        }
-                    }
-                }
-            }
         };
     }
 
@@ -275,61 +196,6 @@ public class ExplosionStone extends FireStone {
             Player player = activePlayer.getPlayer();
             Location location = player.getLocation();
             World world = player.getWorld();
-            double playerX = location.getBlockX();
-            double playerY = location.getBlockY();
-            double playerZ = location.getBlockZ();
-            for (Entity entity : player.getNearbyEntities(4, 4, 4)) {
-                double entityX = entity.getLocation().getBlockX();
-                double entityZ = entity.getLocation().getBlockZ();
-                if (entityX == playerX) {
-                    playerX = entityX + 0.001;
-                }
-                if (entityZ == playerZ) {
-                    playerZ = entityZ + 0.001;
-                }
-                entity.setVelocity(new Vector((double) 1 / (entityX - playerX) * 2, 0.5, (double) 1 / (entityZ - playerZ) * 2));
-            }
-            int playerXint = (int) playerX;
-            int playerYint = (int) playerY;
-            int playerZint = (int) playerZ;
-            if (world.getBlockAt(location.add(0, -1, 0)).getType() != Material.AIR) {
-                StaticVariables.scheduler.scheduleSyncDelayedTask(StaticVariables.plugin, () -> {
-                    location.add(0, 1, 0);
-                    for (int i = 0; i < 3; i++) {
-                        world.getBlockAt(playerXint + 2, playerYint + i, playerZint - 1).setType(Material.STONE);
-                        world.getBlockAt(playerXint + 2, playerYint + i, playerZint).setType(Material.STONE);
-                        world.getBlockAt(playerXint + 2, playerYint + i, playerZint + 1).setType(Material.STONE);
-                        world.getBlockAt(playerXint - 1, playerYint + i, playerZint + 2).setType(Material.STONE);
-                        world.getBlockAt(playerXint, playerYint + i, playerZint + 2).setType(Material.STONE);
-                        world.getBlockAt(playerXint + 1, playerYint + i, playerZint + 2).setType(Material.STONE);
-                        world.getBlockAt(playerXint - 2, playerYint + i, playerZint - 1).setType(Material.STONE);
-                        world.getBlockAt(playerXint - 2, playerYint + i, playerZint).setType(Material.STONE);
-                        world.getBlockAt(playerXint - 2, playerYint + i, playerZint + 1).setType(Material.STONE);
-                        world.getBlockAt(playerXint - 1, playerYint + i, playerZint - 2).setType(Material.STONE);
-                        world.getBlockAt(playerXint, playerYint + i, playerZint - 2).setType(Material.STONE);
-                        world.getBlockAt(playerXint + 1, playerYint + i, playerZint - 2).setType(Material.STONE);
-
-                        world.getBlockAt(playerXint + 1, playerYint + i, playerZint + 1).setType(Material.AIR);
-                        world.getBlockAt(playerXint + 1, playerYint + i, playerZint).setType(Material.AIR);
-                        world.getBlockAt(playerXint + 1, playerYint + i, playerZint - 1).setType(Material.AIR);
-                        world.getBlockAt(playerXint, playerYint + i, playerZint + 1).setType(Material.AIR);
-                        world.getBlockAt(playerXint, playerYint + i, playerZint).setType(Material.AIR);
-                        world.getBlockAt(playerXint, playerYint + i, playerZint - 1).setType(Material.AIR);
-                        world.getBlockAt(playerXint - 1, playerYint + i, playerZint + 1).setType(Material.AIR);
-                        world.getBlockAt(playerXint - 1, playerYint + i, playerZint).setType(Material.AIR);
-                        world.getBlockAt(playerXint - 1, playerYint + i, playerZint - 1).setType(Material.AIR);
-                    }
-                    world.getBlockAt(playerXint + 1, playerYint + 3, playerZint + 1).setType(Material.STONE);
-                    world.getBlockAt(playerXint + 1, playerYint + 3, playerZint).setType(Material.STONE);
-                    world.getBlockAt(playerXint + 1, playerYint + 3, playerZint - 1).setType(Material.STONE);
-                    world.getBlockAt(playerXint, playerYint + 3, playerZint + 1).setType(Material.STONE);
-                    world.getBlockAt(playerXint, playerYint + 3, playerZint).setType(Material.STONE);
-                    world.getBlockAt(playerXint, playerYint + 3, playerZint - 1).setType(Material.STONE);
-                    world.getBlockAt(playerXint - 1, playerYint + 3, playerZint + 1).setType(Material.STONE);
-                    world.getBlockAt(playerXint - 1, playerYint + 3, playerZint).setType(Material.STONE);
-                    world.getBlockAt(playerXint - 1, playerYint + 3, playerZint - 1).setType(Material.STONE);
-                }, 5L);
-            }
         };
     }
 }
