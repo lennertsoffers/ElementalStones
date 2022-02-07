e03fed486afaa9d0058212949fa6b8e937b73456
lennertsoffers <lennert.soffers@hotmail.com>
2022-02-07 19:36:04 +0100

    ExplosionStone: Move7 Effects done

--- src/main/java/com/lennertsoffers/elementalstones/customClasses/models/Effect.java
+++ src/main/java/com/lennertsoffers/elementalstones/customClasses/models/Effect.java
@@ -1,70 +1,290 @@
 package com.lennertsoffers.elementalstones.customClasses.models;
 
-import org.bukkit.Location;
-import org.bukkit.entity.Player;
+import com.lennertsoffers.elementalstones.customClasses.StaticVariables;
+import com.lennertsoffers.elementalstones.customClasses.tools.CheckLocationTools;
+import org.bukkit.*;
+import org.bukkit.entity.*;
+import org.bukkit.inventory.EntityEquipment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.util.Vector;
 
 import java.util.function.Consumer;
-import java.util.function.Function;
 
 public enum Effect {
-    SKELETON_TRAP("skeleton_trap", 200, location -> {
+    SKELETON_TRAP(200, location -> {
+        World world = location.getWorld();
 
+        if (world != null) {
+            ItemStack helmet = new ItemStack(Material.NETHERITE_HELMET, 1);
+
+            for (int i = 0; i < 10; i++) {
+                double locationX = location.getX() + StaticVariables.random.nextGaussian() * 10;
+                double locationZ = location.getZ() + StaticVariables.random.nextGaussian() * 10;
+
+                Location spawnLocation = CheckLocationTools.getClosestAirBlockLocation(new Location(world, locationX, location.getY(), locationZ));
+                if (spawnLocation != null) {
+                    Skeleton skeleton = (Skeleton) world.spawnEntity(spawnLocation, EntityType.SKELETON);
+                    EntityEquipment entityEquipment = skeleton.getEquipment();
+
+                    if (entityEquipment != null) {
+                        entityEquipment.setHelmet(helmet);
+                        entityEquipment.setHelmetDropChance(0);
+                    }
+                }
+            }
+        }
     }),
-    ANGRY_BEES("angry_bees", 200, location -> {
+    ANGRY_BEES(200, location -> {
+        World world = location.getWorld();
 
+        if (world != null) {
+            for (int i = 0; i < 20; i++) {
+                double locationX = location.getX() + StaticVariables.random.nextGaussian() * 10;
+                double locationZ = location.getZ() + StaticVariables.random.nextGaussian() * 10;
+
+                Location spawnLocation = CheckLocationTools.getClosestAirBlockLocation(new Location(world, locationX, location.getY(), locationZ));
+                if (spawnLocation != null) {
+                    Bee bee = (Bee) world.spawnEntity(spawnLocation, EntityType.BEE);
+                    bee.setAnger(12000);
+                }
+            }
+        }
     }),
-    SHULKER_BULLETS("shulker_bullets", 100, location -> {
+    SHULKER_BULLETS(100, location -> {
+        World world = location.getWorld();
 
+        if (world != null) {
+
+            Object[] entities = world.getNearbyEntities(location, 30, 30, 30, entity -> entity instanceof LivingEntity).toArray();
+            if (entities.length > 0) {
+                LivingEntity livingEntity = (LivingEntity) entities[0];
+
+                for (int i = 0; i < 20; i++) {
+                    double locationX = location.getX() + StaticVariables.random.nextGaussian() * 10;
+                    double locationZ = location.getZ() + StaticVariables.random.nextGaussian() * 10;
+
+                    Location spawnLocation = CheckLocationTools.getClosestAirBlockLocation(new Location(world, locationX, location.getY(), locationZ));
+                    if (spawnLocation != null) {
+                        ShulkerBullet shulkerBullet = (ShulkerBullet) world.spawnEntity(spawnLocation, EntityType.SHULKER_BULLET);
+                        shulkerBullet.setTarget(livingEntity);
+                    }
+                }
+            }
+        }
     }),
-    TREE("tree", 100, location -> {
+    TREE(100, location -> {
+        World world = location.getWorld();
 
+        if (world != null) {
+            world.generateTree(location, StaticVariables.random, TreeType.COCOA_TREE);
+        }
     }),
-    ANCIENT_DEBRIS("ancient_debris", 100, location -> {
+    ANCIENT_DEBRIS(100, location -> {
+        World world = location.getWorld();
 
+        if (world != null) {
+            location.getBlock().setType(Material.ANCIENT_DEBRIS);
+        }
     }),
-    DAY_NIGHT("day_night", 100, location -> {
+    DAY_NIGHT(100, location -> {
+        World world = location.getWorld();
 
+        if (world != null) {
+            long time = world.getTime();
+            time += 12000;
+
+            if (time > 24000) {
+                time -= 24000;
+            }
+
+            world.setTime(time);
+        }
     }),
-    WEATHER("weather", 100, location -> {
+    WEATHER(100, location -> {
+        World world = location.getWorld();
 
+        if (world != null) {
+            if (StaticVariables.random.nextBoolean()) {
+                world.setStorm(true);
+            } else {
+                world.setThundering(true);
+                world.setThunderDuration(12000);
+            }
+        }
     }),
-    RANDOM_TELEPORT("random_teleport", 100, location -> {
+    RANDOM_TELEPORT(100, location -> {
+        World world = location.getWorld();
 
+        if (world != null) {
+            world.getNearbyEntities(location, 3, 3, 3, entity -> entity instanceof LivingEntity).forEach(entity -> {
+                LivingEntity livingEntity = (LivingEntity) entity;
+
+                int newX = location.getBlockX() + StaticVariables.random.nextInt(4000) - 2000;
+                int newZ = location.getBlockZ() + StaticVariables.random.nextInt(4000) - 2000;
+                livingEntity.teleport(world.getHighestBlockAt(newX, newZ).getLocation());
+            });
+        }
     }),
-    XP("xp", 100, location -> {
+    XP(100, location -> {
+        World world = location.getWorld();
 
+        if (world != null) {
+            for (int i = 0; i < 20; i++) {
+                ThrownExpBottle thrownExpBottle = (ThrownExpBottle) world.spawnEntity(location, EntityType.THROWN_EXP_BOTTLE);
+                thrownExpBottle.setVelocity(new Vector(StaticVariables.random.nextGaussian(), StaticVariables.random.nextDouble() + 0.5, StaticVariables.random.nextGaussian()));
+            }
+        }
     }),
-    CHARGED_CREEPER("charged_creeper", 100, location -> {
+    CHARGED_CREEPER(100, location -> {
+        World world = location.getWorld();
 
+        if (world != null) {
+            Location spawnLocation = CheckLocationTools.getClosestAirBlockLocation(location);
+            if (spawnLocation != null) {
+                Creeper creeper = (Creeper) world.spawnEntity(spawnLocation, EntityType.CREEPER);
+                creeper.setPowered(true);
+            }
+        }
     }),
-    FISH("fish", 100, location -> {
+    FISH(100, location -> {
+        World world = location.getWorld();
 
+        if (world != null) {
+            for (int i = 0; i < 10; i++) {
+                double locationX = location.getX() + StaticVariables.random.nextGaussian() * 10;
+                double locationZ = location.getZ() + StaticVariables.random.nextGaussian() * 10;
+
+                Location spawnLocation = CheckLocationTools.getClosestAirBlockLocation(new Location(world, locationX, location.getY(), locationZ));
+                if (spawnLocation != null) {
+                    int random = StaticVariables.random.nextInt(3);
+
+                    if (random == 0) {
+                        world.spawnEntity(location, EntityType.COD);
+                    } else if (random == 1) {
+                        world.spawnEntity(location, EntityType.SALMON);
+                    } else {
+                        world.spawnEntity(location, EntityType.TROPICAL_FISH);
+                    }
+                }
+            }
+        }
     }),
-    POTION("potion", 100, location -> {
+    POTION(100, location -> {
+        World world = location.getWorld();
 
+        if (world != null) {
+            ItemStack itemStack = new ItemStack(Material.SPLASH_POTION);
+            PotionMeta potionMeta = (PotionMeta) itemStack.getItemMeta();
+
+            if (potionMeta != null) {
+                PotionEffect potionEffect;
+                
+                int random = StaticVariables.random.nextInt(32);
+                if (random == 0) {
+                    potionEffect = new PotionEffect(PotionEffectType.SPEED, 400, 3, true, true, true);
+                } else if (random == 1) {
+                    potionEffect = new PotionEffect(PotionEffectType.ABSORPTION, 400, 3, true, true, true);
+                } else if (random == 2) {
+                    potionEffect = new PotionEffect(PotionEffectType.BAD_OMEN, 400, 1, true, true, true);
+                } else if (random == 3) {
+                    potionEffect = new PotionEffect(PotionEffectType.BLINDNESS, 400, 3, true, true, true);
+                } else if (random == 4) {
+                    potionEffect = new PotionEffect(PotionEffectType.CONDUIT_POWER, 400, 3, true, true, true);
+                } else if (random == 5) {
+                    potionEffect = new PotionEffect(PotionEffectType.CONFUSION, 1200, 3, true, true, true);
+                } else if (random == 6) {
+                    potionEffect = new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 400, 3, true, true, true);
+                } else if (random == 7) {
+                    potionEffect = new PotionEffect(PotionEffectType.DOLPHINS_GRACE, 400, 3, true, true, true);
+                } else if (random == 8) {
+                    potionEffect = new PotionEffect(PotionEffectType.FAST_DIGGING, 400, 3, true, true, true);
+                } else if (random == 9) {
+                    potionEffect = new PotionEffect(PotionEffectType.FIRE_RESISTANCE, 400, 3, true, true, true);
+                } else if (random == 10) {
+                    potionEffect = new PotionEffect(PotionEffectType.GLOWING, 400, 3, true, true, true);
+                } else if (random == 11) {
+                    potionEffect = new PotionEffect(PotionEffectType.HARM, 400, 3, true, true, true);
+                } else if (random == 12) {
+                    potionEffect = new PotionEffect(PotionEffectType.HEAL, 400, 3, true, true, true);
+                } else if (random == 13) {
+                    potionEffect = new PotionEffect(PotionEffectType.HEALTH_BOOST, 400, 3, true, true, true);
+                } else if (random == 14) {
+                    potionEffect = new PotionEffect(PotionEffectType.HERO_OF_THE_VILLAGE, 400, 3, true, true, true);
+                } else if (random == 15) {
+                    potionEffect = new PotionEffect(PotionEffectType.HUNGER, 400, 3, true, true, true);
+                } else if (random == 16) {
+                    potionEffect = new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 400, 3, true, true, true);
+                } else if (random == 17) {
+                    potionEffect = new PotionEffect(PotionEffectType.INVISIBILITY, 400, 3, true, true, true);
+                } else if (random == 18) {
+                    potionEffect = new PotionEffect(PotionEffectType.JUMP, 400, 3, true, true, true);
+                } else if (random == 19) {
+                    potionEffect = new PotionEffect(PotionEffectType.LEVITATION, 400, 3, true, true, true);
+                } else if (random == 20) {
+                    potionEffect = new PotionEffect(PotionEffectType.LUCK, 400, 3, true, true, true);
+                } else if (random == 21) {
+                    potionEffect = new PotionEffect(PotionEffectType.NIGHT_VISION, 400, 3, true, true, true);
+                } else if (random == 22) {
+                    potionEffect = new PotionEffect(PotionEffectType.POISON , 400, 3, true, true, true);
+                } else if (random == 23) {
+                    potionEffect = new PotionEffect(PotionEffectType.REGENERATION, 400, 3, true, true, true);
+                } else if (random == 24) {
+                    potionEffect = new PotionEffect(PotionEffectType.SLOW_DIGGING, 400, 3, true, true, true);
+                } else if (random == 25) {
+                    potionEffect = new PotionEffect(PotionEffectType.SLOW, 400, 3, true, true, true);
+                } else if (random == 26) {
+                    potionEffect = new PotionEffect(PotionEffectType.SLOW_FALLING, 400, 3, true, true, true);
+                } else if (random == 27) {
+                    potionEffect = new PotionEffect(PotionEffectType.UNLUCK, 400, 3, true, true, true);
+                } else if (random == 28) {
+                    potionEffect = new PotionEffect(PotionEffectType.WATER_BREATHING, 400, 3, true, true, true);
+                } else if (random == 29) {
+                    potionEffect = new PotionEffect(PotionEffectType.WEAKNESS, 400, 3, true, true, true);
+                } else if (random == 30) {
+                    potionEffect = new PotionEffect(PotionEffectType.WITHER, 400, 3, true, true, true);
+                } else {
+                    potionEffect = new PotionEffect(PotionEffectType.SATURATION, 400, 3, true, true, true);
+                }
+
+                potionMeta.addCustomEffect(potionEffect, true);
+
+                ThrownPotion thrownPotion = (ThrownPotion) world.spawnEntity(location.add(0, 1, 0), EntityType.SPLASH_POTION);
+                thrownPotion.setItem(itemStack);
+                thrownPotion.setVelocity(new Vector(0, 0.4, 0));
+            }
+        }
     }),
-    WITHER("wither", 10, location -> {
+    WITHER(10, location -> {
+        World world = location.getWorld();
 
+        if (world != null) {
+            world.spawnEntity(location, EntityType.WITHER);
+        }
     }),
-    DISC_11("disc_11", 1, location -> {
+    DISC_11(1, location -> {
+        World world = location.getWorld();
 
+        if (world != null) {
+            world.playSound(location, Sound.MUSIC_DISC_11, SoundCategory.MUSIC, 100, 100);
+        }
     }),
-    END_MUSIC("end_music", 1, location -> {
+    END_MUSIC(1, location -> {
+        World world = location.getWorld();
 
+        if (world != null) {
+            world.playSound(location, Sound.MUSIC_END, SoundCategory.MUSIC, 100, 100);
+        }
     });
 
-    private final String name;
     private final int chance;
     private final Consumer<Location> effect;
 
-    Effect(String name, int chance, Consumer<Location> effect) {
-        this.name = name;
+    Effect(int chance, Consumer<Location> effect) {
         this.chance = chance;
         this.effect = effect;
-    }
-    
-    public String getName() {
-        return this.name;
     }
     
     public int getChance() {
