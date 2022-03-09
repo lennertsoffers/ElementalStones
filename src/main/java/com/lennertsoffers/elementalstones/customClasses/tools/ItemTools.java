26a72f0d63659521afb97d19f82df98f6fcfe8ff
lennertsoffers <lennert.soffers@hotmail.com>
2022-03-09 11:30:09 +0100

    Fixed bugs - where you cannot spawn a Boss with voodoo doll clear
    - errors when you use a move

---
+++ src/main/java/com/lennertsoffers/elementalstones/customClasses/tools/ItemTools.java
@@ -0,0 +1,27 @@
+package com.lennertsoffers.elementalstones.customClasses.tools;
+
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+
+public class ItemTools {
+
+    public static ItemStack getSingleFromStack(ItemStack itemStack) {
+        ItemStack singleStack = itemStack.clone();
+        singleStack.setAmount(1);
+
+        return singleStack;
+    }
+
+    public static ArrayList<ItemStack> getSingleListFromStackList(ItemStack[] itemStacks) {
+        ArrayList<ItemStack> singleItemCraftingMatrix = new ArrayList<>();
+
+        for (ItemStack itemStack : itemStacks) {
+            if (itemStack != null) {
+                singleItemCraftingMatrix.add(ItemTools.getSingleFromStack(itemStack));
+            }
+        }
+
+        return singleItemCraftingMatrix;
+    }
+}
--- src/main/java/com/lennertsoffers/elementalstones/eventHandlers/InventoryClickEvent.java
+++ src/main/java/com/lennertsoffers/elementalstones/eventHandlers/InventoryClickEvent.java
@@ -1,11 +1,17 @@
 package com.lennertsoffers.elementalstones.eventHandlers;
 
+import com.lennertsoffers.elementalstones.customClasses.tools.ItemTools;
+import com.lennertsoffers.elementalstones.items.CraftItemManager;
 import com.lennertsoffers.elementalstones.items.ItemStones;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.CraftingInventory;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class InventoryClickEvent implements Listener {
 
@@ -14,13 +20,29 @@ public class InventoryClickEvent implements Listener {
         Inventory inventory = event.getClickedInventory();
 
         if (inventory != null) {
-            if (inventory.getType() == InventoryType.WORKBENCH) {
-                if (event.getSlotType() == InventoryType.SlotType.RESULT) {
-                    ItemStack itemInSlot = inventory.getItem(event.getSlot());
 
-                    if (ItemStones.allStones.contains(itemInSlot)) {
-                        inventory.clear();
-                        event.getWhoClicked().setItemOnCursor(itemInSlot);
+            if (inventory.getType() == InventoryType.WORKBENCH || inventory.getType() == InventoryType.CRAFTING) {
+                if (inventory.getType() == InventoryType.WORKBENCH) {
+                    if (event.getSlotType() == InventoryType.SlotType.RESULT) {
+
+                        System.out.println("click");
+
+                        CraftingInventory craftingInventory = (CraftingInventory) inventory;
+                        ItemStack[] craftingMatrix = craftingInventory.getMatrix();
+                        ArrayList<ItemStack> singleItemCraftingMatrix = ItemTools.getSingleListFromStackList(craftingMatrix);
+
+                        if (!Collections.disjoint(CraftItemManager.craftItems, singleItemCraftingMatrix)) {
+                            craftingInventory.setResult(null);
+                        } else if (!Collections.disjoint(ItemStones.allStones, singleItemCraftingMatrix)) {
+                            craftingInventory.setResult(null);
+                        }
+
+                        ItemStack itemInSlot = inventory.getItem(event.getSlot());
+
+                        if (ItemStones.allStones.contains(itemInSlot)) {
+                            inventory.clear();
+                            event.getWhoClicked().setItemOnCursor(itemInSlot);
+                        }
                     }
                 }
             }
